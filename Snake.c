6241fb92c5bdd041967ad0352db2487704cfd918
Mai Duy Dũng <maiduydung@yahoo.com>
2019-09-13 23:16:07 +0700

    Added source.c

---
+++ Snake.c
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
+bool gameover;
+const int width = 20;
+const int height = 20;
+
+int x, y, food_x, food_y, score;
+int tailX[100], tailY[100];
+int nTail;
+enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirecton dir;
+
+void setup() {
+	gameover = false;
+	dir = STOP;
+	
+	x = width / 2; //center
+	y = width / 2;
+
+	food_x = rand() % width;
+	food_y = rand() % height;
+
+	score = 0;
+}
+
+void draw() {
+	system("cls"); //system("clear");
+	for (int i = 0; i < height; i++)
+	{
+		cout << "\n";
+		for (int j = 0; j < width; j++)
+		{
+			// Print @ if this is first row 
+			// or last row. Or this column 
+			// is first or last. 
+			if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+			{
+				cout << "@";
+			}
+			else if (i == x && j == y) // printing the snake head
+				{
+					cout << "o";
+				}
+			else if (i == food_x && j == food_y) 
+				{
+				cout << "x";
+				}
+
+			else
+				cout << " ";
+		}
+	}
+}
+
+
+void logic() {
+	if (_kbhit())
+	{
+		switch (_getch()) 
+		{
+			case 'a':
+				dir = LEFT;
+				break;
+			case 's':
+				dir = DOWN;
+				break;
+			case 'd':
+				dir = RIGHT;
+				break;
+			case 'w':
+				dir = UP;
+				break;
+			case 'z':
+				gameover = TRUE;
+				break;
+		}
+	}
+
+}
+
+void input() {
+
+}
+
+
+void printRectangle(int h, int w)
+{
+	for (int i = 0; i < h; i++)
+	{
+		cout << "\n";
+		for (int j = 0; j < w; j++)
+		{
+			// Print @ if this is first row 
+			// or last row. Or this column 
+			// is first or last. 
+			if (i == 0 || i == h - 1 || j == 0 || j == w - 1)
+				cout << "@";
+			else
+				cout << " ";
+		}
+	}
+}
+int main() {
+	//printRectangle(10, 10);
+	setup();
+	while (!gameover)
+	{
+		draw();
+		input();
+		logic();
+		Sleep(10);
+	}
+
+	return 0;
+}
